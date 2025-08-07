a394b584fa4573d1afa5212890743d0e7e112ba8
Michael Whitley <wavechecker@gmail.com>
2025-08-07 15:54:38 +0400

    Add Netlify adapter & confirm local build success

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  // No integrations needed
+  // …any existing config…
+  adapter: netlify(),
 });
-
