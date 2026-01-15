f2ff2a8096f9df2bfe55a128fb049da63e28b60b
Michael Whitley <wavechecker@gmail.com>
2026-01-15 13:11:30 +0400

    Fix guides: valid getStaticPaths + MDX components

--- src/lib/mdx-components.ts
+++ src/lib/mdx-components.ts
@@ -2,12 +2,7 @@
 import StratBox from "@/components/StratBox.astro";
 import EvidenceBox from "@/components/EvidenceBox.astro";
 
-// Add other MDX components you embed in content here as you introduce them.
-// Example:
-// import OutbreakTracker from "@/components/OutbreakTracker.astro";
-
 export const mdxComponents = {
   StratBox,
   EvidenceBox,
-  // OutbreakTracker,
 };
